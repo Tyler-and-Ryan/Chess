51306db2addda903075dc4bccba18ef0210e70d4
Magicboys <49106114+Magicboys@users.noreply.github.com>
2020-06-16 13:23:26 -0700

    version 0.01

---
+++ src/main.java
@@ -0,0 +1,11 @@
+
+// Main client
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First message");
+		String hi = "hi";
+	}
+
+}
