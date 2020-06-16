d3c704a776d0c70ee76a443f3dc11af0c8f3903c
Magicboys <49106114+Magicboys@users.noreply.github.com>
2020-06-16 13:38:16 -0700

    better update
    yes

--- src/main.java
+++ src/main.java
@@ -2,13 +2,10 @@
 // Main client
 public class main {
 
-	//RED teddy is a loser
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First message");
-		System.out.println("broski");
-		System.out.println("red teddy");
-		System.out.println("death");
+		System.out.println("Brown teddy has ligma and sugma");
 		
 	}
 
