205f9695f5d375b5bb915cabefd7fc65129583d7
ILikeMalware425 <66922150+ILikeMalware425@users.noreply.github.com>
2020-06-16 14:29:04 -0700

    Pawn Update again *anagry*

--- src/Pawn.java
+++ src/Pawn.java
@@ -1,6 +1,35 @@
 
 public class Pawn extends Object {
-	Pawn(boolean val){
-		
+	boolean player;
+	int moveCounter;
+	String pieceName;
+	
+	//Pawn constructor that tracks:
+	//Which player the pawn is controlled by (true = P1, false = P2)
+	//How many times the pawn has moved (moveCounter) 
+	//The name 'Pawn' stored in a string
+	public Pawn(boolean player) {
+		this.player = player;
+		moveCounter = 0;
+		pieceName = "Pawn";
+	}
+	
+	//overrides java toString and returns the 'Pawn'
+	public String toString() {
+		return pieceName;
+	}
+
+	//returns the amount of times this pawn has moved within the one game 
+	public int moveCount() {
+		return moveCounter;
+	}
+	
+	//returns which player this pawn is owned by
+	public boolean getPlayer() {
+		return player;
+	}
+	
+	public void moved() {
+		moveCounter++;
 	}
 }
