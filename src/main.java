ef81f8933006be4984fdc90dea008009ca818762
ILikeMalware425 <66922150+ILikeMalware425@users.noreply.github.com>
2020-06-16 13:35:04 -0700

    useless update
    1 comment, 3 important messages

--- src/main.java
+++ src/main.java
@@ -2,9 +2,14 @@
 // Main client
 public class main {
 
+	//RED teddy is a loser
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First message");
+		System.out.println("broski");
+		System.out.println("red teddy");
+		System.out.println("death");
+		
 	}
 
 }
