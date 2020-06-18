3886b9032ea6cb42e61e593b2491ff72a91fec90
ILikeMalware425 <66922150+ILikeMalware425@users.noreply.github.com>
2020-06-18 16:37:41 -0700

    Update Object.java

--- src/Object.java
+++ src/Object.java
@@ -1,26 +1,6 @@
 
 public class Object {
 	
-	//this is the object parent class since each game piece is a type of object
-	public Object() {
-	}
 	
-	//overrides java toString and returns the 'Pawn'
-	public String toString() {
-		return "null";
-	}
-
-	public boolean isGamePiece() {
-		return false;
-	}
-	
-	public int moveCount() {
-		return -1;
-	}
-	//this method serves no purpose other than appeasing eclipse
-	//if this method is being called during the logic of an isLegal method, something is wrong
-	public boolean getPlayer() {
-		return false;
-	}
 	
 }
