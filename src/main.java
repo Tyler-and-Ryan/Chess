58234e3cf6babc4cb5f6ee9348151feb0629a12c
Magicboys <49106114+Magicboys@users.noreply.github.com>
2020-06-22 18:39:20 -0700

    Update main.java

--- src/main.java
+++ src/main.java
@@ -9,8 +9,14 @@ public class main {
 		System.out.println(game1.toString());
 		//game1.GameStats();
 		System.out.println("====================================");
-		game1.MovePiece(1, 0, 2, 0, false);
+		//game1.MovePiece(1, 0, 2, 0, false);
+		game1.RemovePiece(1, 0);
 		System.out.println(game1.toString());
+		game1.MovePiece(0, 0, 6, 0, true);
+		System.out.println(game1.toString());
+		game1.MovePiece(6, 0, 6, 1, true);
+		System.out.println(game1.toString());
+		game1.ClearBoard();
 		//game1.GameStats();
 		
 	}
