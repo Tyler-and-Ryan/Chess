3ce31b14eb8f93dc9e0c838a5ec1e8ac09c52e77
ILikeMalware425 <66922150+ILikeMalware425@users.noreply.github.com>
2020-06-22 21:02:41 -0700

    Correct spacing

--- src/main.java
+++ src/main.java
@@ -28,32 +28,32 @@ public class main {
 		}
 	}
 
-		//interprets the user input containing a letter representing a column and a number representing a row and converts it to row/col ints
-		public void attemptMove(String currentPieceLoc, String moveToLoc) {
-			int row;
-			int col;
-			int moveToRow;
-			int moveToCol;
-			
-			if(currentPieceLoc.substring(0,1).equals("A")) {
-				col = 0;
-			} else if(currentPieceLoc.substring(0,1).equals("B")) {
-				col = 1;
-			} else if(currentPieceLoc.substring(0,1).equals("C")) {
-				col = 2;
-			} else if(currentPieceLoc.substring(0,1).equals("D")) {
-				col = 3;
-			} else if(currentPieceLoc.substring(0,1).equals("E")) {
-				col = 4;
-			} else if(currentPieceLoc.substring(0,1).equals("F")) {
-				col = 5;
-			} else if(currentPieceLoc.substring(0,1).equals("G")) {
-				col = 6;
-			} else if(currentPieceLoc.substring(0,1).equals("H")) {
-				col = 7;
-			} else {
-				System.out.println("Unrecognized input, try again");
-				return;
-			}
+	//interprets the user input containing a letter representing a column and a number representing a row and converts it to row/col ints
+	public void attemptMove(String currentPieceLoc, String moveToLoc) {
+		int row;
+		int col;
+		int moveToRow;
+		int moveToCol;
+		
+		if(currentPieceLoc.substring(0,1).equals("A")) {
+			col = 0;
+		} else if(currentPieceLoc.substring(0,1).equals("B")) {
+			col = 1;
+		} else if(currentPieceLoc.substring(0,1).equals("C")) {
+			col = 2;
+		} else if(currentPieceLoc.substring(0,1).equals("D")) {
+			col = 3;
+		} else if(currentPieceLoc.substring(0,1).equals("E")) {
+			col = 4;
+		} else if(currentPieceLoc.substring(0,1).equals("F")) {
+			col = 5;
+		} else if(currentPieceLoc.substring(0,1).equals("G")) {
+			col = 6;
+		} else if(currentPieceLoc.substring(0,1).equals("H")) {
+			col = 7;
+		} else {
+			System.out.println("Unrecognized input, try again");
+			return;
 		}
+	}
 }
