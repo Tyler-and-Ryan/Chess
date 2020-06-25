a5bf9adbfa08dd08ae3014daedb5f2dbde371583
Magicboys <49106114+Magicboys@users.noreply.github.com>
2020-06-24 21:33:08 -0700

    More canvas stuff

--- src/Display.java
+++ src/Display.java
@@ -2,36 +2,47 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Display {
-	Frame board;
+	Frame canvas;
 	
 	public Display() {
-		board = new Frame();
-		board.setSize(500,500);
-		board.setVisible(true);
+		canvas = new Frame();
+		canvas.setSize(800,800);
+		canvas.setVisible(true);
 		
 		//Menu bar
+		MenuBar();
 		
+		//Sets up game board
+		Panel gameboard = new Panel();
+		gameboard.setSize(400,400);
+		gameboard.setBounds(10, 10, 300, 300);
+		Label playerOneName = new Label("Player One");
+		Label playerTwoName = new Label("Player Two");
+		gameboard.add(playerTwoName);
+		gameboard.add(playerOneName);
+		Color background = new Color(808080);
+		gameboard.setBackground(background);
+		canvas.add(gameboard);
+		
+		canvas.setVisible(true);
+		
+		
+	}
+
+	//Handles creation and actions for the menu bar
+	public void MenuBar() {
 		MenuBar panel = new MenuBar();
 		Menu menu = new Menu("Options");
 		MenuItem quit = new MenuItem("quit");
 		menu.add(quit);
 		panel.add(menu);
-		board.setMenuBar(panel);
+		canvas.setMenuBar(panel);
 		
-		//action listener
+		//Adds quit option functionality
 		quit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				board.setVisible(false);
+				canvas.setVisible(false);
 			}
 		});
-		
-		board.setVisible(true);
-		
-		
-	}
-
-	
-	public void CloseBoard() {
-		board.setVisible(false);
 	}
 }
