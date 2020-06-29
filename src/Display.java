94b288466d5fcccb9931c527b1f1161e258be843
Magicboys <49106114+Magicboys@users.noreply.github.com>
2020-06-28 23:34:44 -0700

    Display update
    - Colored the pieces depending on which player it is
    - empty squares now have no text
    - squares now have a background color

--- src/Display.java
+++ src/Display.java
@@ -21,19 +21,21 @@ public class Display {
 		MenuBar();
 		
 		//Sets up game board
+		Color background = new Color(74,102,104);
 		gameboard = new Container();
-		gameboard.setSize(700,700);
-		
+		//gameboard.setSize(800,800);
 		RefreshBoard();
 		
+		//Adds player names to the board
 		JLabel playerTwoName = new JLabel("Player Two");
 		canvas.add(playerTwoName);
-		playerTwoName.setBounds(350, 0, 100, 20);
+		playerTwoName.setBounds((width/2)-50, 0, 100, 20);
 		
 		JLabel playerOneName = new JLabel("Player One");
 		canvas.add(playerOneName);
-		playerOneName.setBounds(350, 700, 100, 20);
+		playerOneName.setBounds((width/2)-50, (height-250), 100, 20);
 		
+		//Makes the gameboard visible once set up is complete
 		canvas.setLayout(null);
 		canvas.setVisible(true);
 		
@@ -49,56 +51,74 @@ public class Display {
 	public void RefreshBoard() {
 		Container refresh = new Container();
 		
-		refresh.setSize(700,700);
 		
 		for(int i = 8; i >= 0; i--) {
 			for(int j = 0; j <= 8; j++) {
-				JLabel temp = null;
+				JButton temp = null;
+				JLabel border = null;
 				
 				//Adds appropriate box content to the label which will later be inserted into the grid
 				if(j == 0 && i != 0) {
-					temp = new JLabel("Row " + i);
+					border = new JLabel("Row " + i);
 				} else if (i != 0) {
 					
 					//Game Content Squares
 					if(game.GetPiece(i-1, j-1) != null) {
-						temp = new JLabel(game.GetPiece(i-1,j-1).toString());
+						temp = new JButton(game.GetPiece(i-1,j-1).toString());
 					} else {
-						temp = new JLabel("Empty");
+						temp = new JButton("Empty");
 					}
 				}
 				if(i == 0) {
 					if(j == 0) {
-						temp = new JLabel("");
+						border = new JLabel("");
 					} else if(j == 1) {
-						temp = new JLabel("Column A");
+						border = new JLabel("Column A");
 					} else if (j == 2) {
-						temp = new JLabel("Column B");
+						border = new JLabel("Column B");
 					} else if (j == 3) {
-						temp = new JLabel("Column C");
+						border = new JLabel("Column C");
 					} else if (j == 4) {
-						temp = new JLabel("Column D");
+						border = new JLabel("Column D");
 					} else if (j == 5) {
-						temp = new JLabel("Column E");
+						border = new JLabel("Column E");
 					} else if (j == 6) {
-						temp = new JLabel("Column F");
+						border = new JLabel("Column F");
 					} else if (j == 7) {
-						temp = new JLabel("Column G");
+						border = new JLabel("Column G");
 					} else if (j == 8) {
-						temp = new JLabel("Column H");
+						border = new JLabel("Column H");
 					}
 				}
 				
 				//Adds box content to board
-				refresh.add(temp);
+				if(temp != null) {
+					Color background = new Color(74,102,104);
+					if(game.GetPiece(i-1, j-1) == null) {
+						temp.setText("");
+						temp.disable();
+					} else if(game.GetPiece(i-1, j-1).getPlayer() == true) {
+						Color playerOne = new Color(102, 255, 51);
+						temp.setForeground(playerOne);
+					} else {
+						Color playerTwo = new Color(255, 51, 51);
+						temp.setForeground(playerTwo);
+					}
+					temp.setBackground(background);
+					refresh.add(temp);
+				} else {
+					refresh.add(border);
+				}
 			}
 
 		}
 		
 		gameboard.setVisible(false);
 		refresh.setLayout(new GridLayout(9,9));
+		Color background = new Color(74,102,104);
+		refresh.setBackground(background);
 		canvas.add(refresh);
-		refresh.setBounds(100, 50, 600, 600);
+		refresh.setBounds(100, 50, 700, 700);
 		canvas.validate();
 		gameboard = refresh;
 		
