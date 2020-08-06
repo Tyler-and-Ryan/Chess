10c6a09e5fd041d199bdfe9517d019538d9bdd6c
Magicboys <49106114+Magicboys@users.noreply.github.com>
2020-08-05 20:38:42 -0700

    AI Seems smarter

--- src/Alexa.java
+++ src/Alexa.java
@@ -33,37 +33,64 @@ public class Alexa {
 		//position 1 is the new row/col
 		Point[] move = new Point[2];
 		
-		Random rand = new Random();
-		boolean validMove = false;
-		
 		Object piece = null;
-		ArrayList<Point> options = null;
 		
+		ArrayList<Point[]> options = new ArrayList<Point[]>();
 		
+		//gets all the possible legal moves available
+		Object[] AIPieces = game.getPlayerTwoPieces();
+		for(int i = 0; i < AIPieces.length; i++) {
+			piece = AIPieces[i];
 		
-		//Gets a piece on the board to move
-		boolean goodPiece = false;
-		while(goodPiece == false) {
-			int pieceNumber = rand.nextInt(16);
-			Object[] AIPieces = game.getPlayerTwoPieces();
-			piece = AIPieces[(pieceNumber%AIPieces.length)];
-					
 			if(piece.GetStatus() == true && !game.refreshCheck(false)) {
-				options = game.LegalMoves(piece.GetRow(), piece.GetCol());
-				if(options != null) {
-					goodPiece = true;
+				ArrayList<Point> legalMoves = game.LegalMoves(piece.GetRow(), piece.GetCol());
+				if(legalMoves != null) {
+					for(int j = 0; j < legalMoves.size(); j++) {
+						Point[] temp = new Point[2];
+						temp[0] = new Point(piece.GetRow(), piece.GetCol());
+						temp[1] = legalMoves.get(j);
+						options.add(temp);
+					}
 				}
 			}
 		}
-			
-		//Gets location of new move and original location
-		Point original = new Point(piece.GetRow(),piece.GetCol());
-		int randomOption = rand.nextInt(options.size());
-		System.out.println("OPTIONS: " + options.size());
-		Point moveLocation = options.get(randomOption%options.size());
 		
-		move[0] = original;
-		move[1] = moveLocation;
+		//sorts the best legal move to position 0 in the arraylist
+		int maxScore = 0;
+		for(int i = 0; i < options.size(); i++) {
+			Point[] moveTemp = options.get(i);
+			int row = moveTemp[1].x;
+			int col = moveTemp[1].y;
+			int moveScore = 0;
+			
+			if(game.GetPiece(row,col) != null) {
+				if(game.GetPiece(row, col).toString() == "Pawn") {
+					moveScore = PAWN;
+				} else if (game.GetPiece(row, col).toString() == "Queen") {
+					moveScore = QUEEN;
+				} else if (game.GetPiece(row, col).toString() == "Castle") {
+					moveScore = CASTLE;
+				} else if (game.GetPiece(row, col).toString() == "Bishop") {
+					moveScore = BISHOP;
+				} else if (game.GetPiece(row, col).toString() == "Horse") {
+					moveScore = HORSE;
+				} else if (game.GetPiece(row, col).toString() == "King") {
+					moveScore = KING;
+				} 
+			}
+			
+			if(moveScore > maxScore) {
+				moveScore = maxScore;
+				options.add(0, options.get(i));
+				options.remove(options.get(i+1));
+			}
+			
+		}
+		
+		//Gets the origin of the best move and the final destination for the move
+		move = options.get(0);
+		
+		moveCounter++;
 		
 		return move;
 	}
